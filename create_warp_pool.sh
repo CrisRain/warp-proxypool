9678d4db82adf12ed388d4d94eb014a9996d43d4
CrisRain <break_meteor@outlook.com>
2025-06-06 23:23:12 +0800

    修正 create_pool 函数中的输出格式，优化等待状态提示信息的可读性

--- create_warp_pool.sh
+++ create_warp_pool.sh
@@ -234,7 +234,7 @@ create_pool() {
                         _SVC_READY=true
                         break
                     fi
-                    echo "       等待中... (尝试 $(($_SVC_WAIT_COUNT + 1))/$_MAX_SVC_WAIT_ATTEMPTS)"
+                    echo "       等待中... 尝试 $(($_SVC_WAIT_COUNT + 1)) / $_MAX_SVC_WAIT_ATTEMPTS"
                     sleep 2
                     _SVC_WAIT_COUNT=$(($_SVC_WAIT_COUNT + 1))
                 done
